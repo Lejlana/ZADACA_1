980d12dbb1cb558375ae7e3282379dc32ef55a0d
Lejlana <lelabeganovic@gmail.com>
2023-03-22 20:33:19 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //String someText = "Some random text"
+       //int someNumber = someText;
+
+        int someRandomInt = 1;
+
+
+
+
+
+        System.out.println(someRandomInt);
     }
 }

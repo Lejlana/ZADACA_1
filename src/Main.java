cbfaa7f38c4d2620d8a1184d54062f559735f921
Lejlana <lelabeganovic@gmail.com>
2023-03-25 15:10:57 +0100

    Zadaca_1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,74 @@
+import java.time.Year;
+
 public class Main {
     public static void main(String[] args) {
-        //String someText = "Some random text"
-       //int someNumber = someText;
+        //Zadatak 1.
 
-        int someRandomInt = 1;
+        //String someText = "SomeRandomText";
+        //int SomeNumber = "someText";
+        //int someRandomNumber = 5265;
+
+        //Zadatak 2.
+
+        String nameAndSurname = "Lejla Softic";
+        System.out.println(nameAndSurname);
+
+        char randomCharacter = 'L';
+        System.out.println(randomCharacter);
+
+        int YearOfBirth = 1988;
+        System.out.println(YearOfBirth);
+
+        long SomeLongNumber = 100000000000L;
+        System.out.println(SomeLongNumber);
+
+
+        double SomeNumber = 42.001;
+        System.out.println(SomeNumber);
+
+        //Zadatak 3.
+
+        char firstNameLetter = 76;
+        char secondNameLetter = 69;
+        char thirdNameLetter = 74;
+        char fourthNameLetter = 76;
+        char fifthNameLetter = 65;
+        System.out.println(firstNameLetter+""+secondNameLetter+""+thirdNameLetter+""+fourthNameLetter+""+fifthNameLetter);
+
+        //Zadatak 4.
+
+        String UltimateQuestion = "Answer to the Ultimate Question of Life, the Universe, and Everything is the number 42:";
+        boolean UltimateAnswer = true;
+        System.out.println(UltimateQuestion + (UltimateAnswer));
+
+        UltimateQuestion = "Answer to the Ultimate Question of Life, the Universe, and Everything is the number 42:";
+        UltimateAnswer = false;
+        System.out.println(UltimateQuestion + (UltimateAnswer));
+
+        //Zadatak 5.
+        //This is a number xxx and it is about to double;
+
+        int NumberToDouble = 17;
+        int Multiplier = 2;
+        System.out.println(NumberToDouble * Multiplier);
+        Multiplier = 2;
+        System.out.println(NumberToDouble * Multiplier);
+        Multiplier = 2;
+        System.out.println(NumberToDouble * Multiplier);
+        Multiplier = 2;
+        System.out.println(NumberToDouble * Multiplier);
+        Multiplier = 2;
+        System.out.println(NumberToDouble * Multiplier);
+        Multiplier = 2;
+        System.out.println(NumberToDouble * Multiplier);
+        Multiplier = 2;
+        System.out.println(NumberToDouble * Multiplier);
+        Multiplier = 2;
+        System.out.println(NumberToDouble * Multiplier);
 
 
 
 
-
-        System.out.println(someRandomInt);
     }
 }
+
